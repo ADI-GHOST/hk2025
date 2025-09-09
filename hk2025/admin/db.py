c74fb2a201b08df37b988f7e979233584b8478fc
aditya anand <aditya68523490@gmail.com>
2025-09-09 18:15:40 +0530

    Add files via upload

---
+++ hk2025/admin/db.py
@@ -0,0 +1,18 @@
+import mysql.connector
+from mysql.connector import Error
+
+def create_connection():
+    """Create and return a database connection object."""
+    
+    connection = mysql.connector.connect(
+            host='localhost', # Replace with your host
+            database='adm',
+            user='root', # Replace with your username
+            password='aditya',
+            auth_plugin='mysql_native_password'
+              
+        )
+    if connection.is_connected():
+            return connection
+  
+
